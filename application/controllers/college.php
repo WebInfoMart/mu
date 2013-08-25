2e99ccbc17a968c65737c5b7ebcd32efc2443830
Deepak Sharma <deepaks@webinfomart.com>
2013-08-25 11:27:48 +0530

    College page

--- application/controllers/college.php
+++ application/controllers/college.php
@@ -6,17 +6,27 @@ class College extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->library('layout');
-		//$this->load->model('univ_model');
+		$this->load->model('collegeModel');
+		$this->load->library("pagination");
 	}
-	function index()
+public function index() {
+        $config = array();
+        $config["base_url"] = base_url() . "college/college";
+        $config["total_rows"] = $this->collegeModel->record_count();
+        $config["per_page"] = 10;
+        $config["uri_segment"] = 3;
+		$config['full_tag_open'] = '<div id="pagination">';
+		$config['full_tag_close'] = '</div>';
+        $this->pagination->initialize($config);
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+        $data["results"] = $this->collegeModel-> getAllUniversities($config["per_page"], $page);
+		$data["links"] = $this->pagination->create_links();
+		$data["countResults"] = $this->collegeModel-> record_count();
+        $this->layout->view("college/college", $data);
+    }
+	public function individualCollege($collegeId)
 	{
-		/* $data=array();
-		if($this->input->post('searchUniv'))
-		{
-			$data['university']=$this->univ_model->search_univ();
-		}
-		$this->load->view('university/search',$data); */
-		$this->layout->view('college/college');
-		
+	 $data["universityData"]=$this->collegeModel->getUniversityDetailById($collegeId);
+	 $this->layout->view("college/individualCollege",$data);
 	}
 }
